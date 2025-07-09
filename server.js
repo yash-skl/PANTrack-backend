f7188854f30b2b530ea3119273fd7a4c5efd705d
Yash Shukla <yshukla2775@gmail.com>
2025-07-09 15:59:39 +0530

    .....

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 
 
-const allowedOrigins = process.env.CLIENT_URL || "http://localhost:5173";
+const allowedOrigins = ["https://pan-track-frontend-cchb.vercel.app/" , "http://localhost:5173"];
 app.use(
     cors({
         origin: allowedOrigins,
