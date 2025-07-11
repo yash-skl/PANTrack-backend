87020685a6f50f00b076569204e19a8f476c504a
Yash Shukla <yshukla2775@gmail.com>
2025-07-11 06:38:39 +0530

    ..

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 
 
-const allowedOrigins = ["https://pan-track-frontend-tbin.vercel.app" , "http://localhost:5173"];
+const allowedOrigins = ["https://pan-track-frontend-pt1g.vercel.app" , "http://localhost:5173"];
 app.use(
     cors({
         origin: allowedOrigins,
